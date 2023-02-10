16bb4c87006edfadb81a2c5895346b710ecd7c6d
Arjun Balaji <95229197+ArjunNo1@users.noreply.github.com>
2023-02-10 10:35:57 +0530

    CodeWork

---
+++ Microsoft/Airplane_Seat_Assignment_Probability_1227.java
@@ -0,0 +1,10 @@
+public class Airplane_Seat_Assignment_Probability_1227 {
+    public double nthPersonGetsNthSeat(int n) {
+        if (n == 1) {
+            return 1.00000;
+        } else {
+            return 0.50000;
+        }
+    }
+
+}
